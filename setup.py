fde978110782d3ebc26f388f67147bf9c6d009bb
jhonattansousa <95jhonatannn@gmail.com>
2024-05-06 13:01:54 +0000

    Commit realizado a partir da branch Main do executavel do T2Extractor. https://github.com/T2C-Consultoria/prj_QUAL_T2Extractor_Lib

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='t2extractor',
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "boto3>=1.34.84",
+        "openpyxl>=3.1.2",
+        "pdf2image>=1.17.0",
+        "pyodbc>=5.1.0",
+        "PyPDF2>=3.0.1",
+        "requests>=2.31.0",
+    ],
+)
